ba614aa1556b941f8ccff2e72c2ef179c674b488
Peikai Guo <peikaiguo7@gmail.com>
2016-01-19 15:21:47 +1300

    CSV file generation

---
+++ CSV/HomeController.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Web.Mvc;
+using Demo.Models;
+using FileHelpers;
+
+namespace Demo.Controllers
+{
+    public class HomeController : Controller
+    {
+        public FileResult Csv()
+        {
+            var data = new StringBuilder();
+            data.AppendLine("Id,Name");
+            foreach (var product in getProducts())
+                data.AppendLine($"{product.Id},{product.Name}");
+            var bytes = Encoding.UTF8.GetBytes(data.ToString());
+            return File(bytes, "text/csv", "Products.csv");
+        }
+
+        public FileResult FileHelperCsv()
+        {
+            var engine = new FileHelperEngine<Product> {HeaderText = "Id,Name"};
+            var data = engine.WriteString(getProducts());
+            var bytes = Encoding.UTF8.GetBytes(data);
+            return File(bytes, "text/csv", "Products.csv");
+        }
+
+        private List<Product> getProducts()
+        {
+            return Enumerable.Range(0, 1000).Select(x => new Product {Id = x, Name = $"Name {x}"}).ToList();
+        }
+    }
+}
---
+++ CSV/Product.cs
@@ -0,0 +1,13 @@
+﻿using FileHelpers;
+
+namespace Demo.Models
+{
+    [DelimitedRecord(",")]
+    public class Product
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string PhotoUrl { get; set; }
+        public string ThumbnailUrl { get; set; }
+    }
+}
